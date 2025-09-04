6d510b664402d47b17f2fab83c3283a76f25b4dd
HDElectronics <khadraouiibrahim@gmail.com>
2025-09-04 12:16:26 -0700

    Refactor const_config.py to load environment variables for configuration

--- tests/client/const_config.py
+++ tests/client/const_config.py
@@ -14,13 +14,17 @@
     permissions and limitations under the License.
  """
 
+import os
+from pydotenv import load_dotenv
 from compreface.config.api_list import DETECTION_API
 
+load_dotenv()
 
-DOMAIN: str = 'http://localhost'
-PORT: str = '8000'
-RECOGNIZE_API_KEY: str = '9916f5d1-216f-4049-9e06-51c140bfa898'
+
+DOMAIN: str = os.getenv('DOMAIN')
+PORT: str = os.getenv('PORT')
+RECOGNIZE_API_KEY: str = os.getenv('RECOGNITION_API_KEY')
+DETECTION_API_KEY: str = os.getenv('DETECTION_API_KEY')
+VERIFICATION_API_KEY: str = os.getenv('VERIFICATION_API_KEY')
 FILE_PATH: str = "tests/common/jonathan-petit-unsplash.jpg"
 IMAGE_ID: str = "image-id"
-DETECTION_API_KEY: str = 'a482a613-3118-4554-a295-153bd6e8ac65'
-VERIFICATION_API_KEY: str = '3c6171a4-e115-41f0-afda-4032bda4bfe9'
